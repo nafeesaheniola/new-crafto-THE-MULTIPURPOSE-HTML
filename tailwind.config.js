1181b4c4f7e90c1a68598f33ccb11d9ea2fe467f
Adebayo <tadebayo@dervac.com>
2024-03-17 22:48:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'headerbg': "url('/src/images/Business-man.jpg')",
+        
+      }
+    },
+  },
+  plugins: [],
+}
